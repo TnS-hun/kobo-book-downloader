5e8a6095ee6e6cc3ca5d3d6a61b34869a6a6fb55
Brandon Davis <brandon.davis@kitware.com>
2020-04-04 17:56:48 -0400

    [docs] add README to pypi description

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ import os
 
 from setuptools import find_packages, setup
 
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='kobodl',
     author='Brandon Davis',
-    version='0.3.1',
+    version='0.3.3',
     author_email='kobodl@subdavis.com',
     url="https://github.com/subdavis/kobo-book-downloader",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
