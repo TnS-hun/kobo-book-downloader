902b8f1012b9b7931de83d881aee0eed2e7e8d4d
TnS-hun <TnS-hun@users.noreply.github.com>
2022-04-11 13:03:16 +0200

    [fixed] Fake the user agent string because the Kobo API stopped responding, it just held the connection.

--- kobo-book-downloader/Kobo.py
+++ kobo-book-downloader/Kobo.py
@@ -58,6 +58,7 @@ class Kobo:
 	def __init__( self ):
 		self.InitializationSettings = {}
 		self.Session = SessionWithTimeOut()
+		self.Session.headers.update( { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0" } )
 
 	# This could be added to the session but then we would need to add { "Authorization": None } headers to all other
 	# functions that doesn't need authorization.
